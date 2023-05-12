90b0ba05b19d341770802a220053958295e55d25
Uri Shani <uri.shani@gmail.com>
2023-05-12 10:08:21 +0300

    functionality works reasonably

--- simulator/sailboat.js
+++ simulator/sailboat.js
@@ -1,11 +1,12 @@
     // Constants
     const KN = 10; // Constant for normal sail power
     const KT = 10; // Constant for tangential sail power
-    const KW = 10; // Constant for angular change rate calculation
-    const M = 100; // Boat mass in kg
+    const KW = 1; // Constant for angular change rate calculation
+    const M = 10; // Boat mass in kg
     const L = 5; // Boat length in meters
-    const DT = 1/60; // Time interval in seconds
-    const R = 0.5; // Water resistance constant
+    const KB = 1; // factorfor wind effect on the boat body;
+    let DT = 1/60; // Time interval in seconds, but it is acrually compured in updateBoat()
+    const R = [-1.0,0,0.5]; // Water resistance constant [backward, still, forward]
     const boatLength = 170;
     const mastPosition = 20;
     const sailLength = 110;
@@ -17,9 +18,12 @@
     let steeringAngle = 0;
     let boatDirection = Math.PI/2; //Math.PI/4; // Angle in radians
     let boatSpeed = 0; // Meters per second
+    let boatDrift = 0; // Meters per second boat drifting sideways.
     let windDirection = Math.PI/2; // Angle in radians
     let windSpeed = 30; // Meters per second
     let maxWindSpeed = 10; // Meters per second
+    let keelPosition = 0; // 0 = up, 1 = middle, 2 = down
+    let boatPosition = { x: 0, y: 0 }; // Boat position in meters
     // Canvas setup
     let canvas, ctx, centerX, centerY, boat_speed_text; // context variables to be set on initialization
     
@@ -105,31 +109,43 @@ function drawFullBoat() {
 }
     
 // Displaying data on screen methods:
-function X(w,v) {
-    document.getElementById(w).value = (v/Math.PI * 180).toFixed(2);
+function X(w,v, v2) {
+    document.getElementById(w).value = (v/Math.PI * 180).toFixed(1) + 
+        (v2 === undefined ? "" : (" / " + (v2/Math.PI * 180).toFixed(1)));
 }
-function Y(w,v) {
-    document.getElementById(w).value = v.toFixed(2);
+function Y(w,v, v2) {
+    document.getElementById(w).value = v.toFixed(1) +
+        (v2 === undefined ? "" : (" / " + v2.toFixed(1)));
 }
 const displayWindAngle = (v) => X('windAngle',v);
 const displayWindSpeed = (v) => Y('windSpeed',v/10);
 const displaySailAngle = (v) => X('sailAngle',v);
 const displaySteeringAngle = (v) => X('steeringAngle',v);
 const displayBoatDirection = (v) => X('boatDirection',v);
-const displayBoatSpeed = (v) => Y('boatSpeed',v);
+const displayBoatSpeed = (speedF, speedD) => Y('boatSpeed',speedF, speedD);
+const displayBoatPosition = (posx, posy) => Y('boatPosition',posx, posy);
 const displayWindBoatAngle = (v) => X('windBoatAngle',v);
 const displayWindSailAngle = (v) => X('windSailAngle',v);
-const displaySailForce = (v) => Y('sailForce',v);
+const displayForce = (fwdF, driftF) => Y('windForce',fwdF, driftF);
 const displayWaterResistance = (v) => Y('waterResistance',v);
+const displayAcceleration = (v) => Y('acceleration',v);
+const displayRudderTorque = (v) => Y('rudderTorque',v);
 
 // Update boat direction and speed
 let boatIncrement = 0;
+let lastTime = new Date().getTime();
+function condition(v, min, max) {
+    return v >= min && v <= max ? 1 : 0;
+}
 function updateBoat() {
-    boatDirection = (boatDirection += boatIncrement)% (2*Math.PI);
+    let now = new Date().getTime();
+    DT = (now - lastTime)/1000;
+    lastTime = now;
+    // boatDirection = (boatDirection + boatIncrement)% (2*Math.PI);
     // Compute angle between wind and boat direction
     let windAngle = (windDirection - boatDirection);
-    if (windAngle > Math.PI) windAngle = windAngle - 2*Math.PI;
-    let windSailAngle = (sailAngle < 0 ? -1:1) * (sailAngle - windAngle);
+    if (windAngle > Math.PI) windAngle = windAngle - 2*Math.PI; // wind to boat angle is positive on the right sie, negative on the left side.
+    let windSailAngle = Math.sign(sailAngle) * (sailAngle - windAngle); // positive on the left sideof sail, negative on the right (back) side
     if (windSailAngle > Math.PI) windSailAngle = windSailAngle - 2*Math.PI;
     displayWindSailAngle(windSailAngle);
     displayWindAngle(windDirection); 
@@ -137,16 +153,33 @@ function updateBoat() {
     displayBoatDirection(boatDirection); 
     displayWindBoatAngle(windAngle);
     // Compute sail and rudder forces
-    const sailForce = KN * Math.abs(Math.cos(windSailAngle)) + KT * Math.abs(Math.sin(windSailAngle));
-    const waterResistance = R * boatSpeed * boatSpeed;
-    displaySailForce(sailForce);
+    const sailForce = Math.sign(windSailAngle) * windSpeed * (
+        KT * Math.abs(Math.cos(windSailAngle))* condition(windSailAngle, 140, 170) + 
+        KN * Math.abs(Math.sin(windSailAngle))
+    );
+    const fwdSailForce = sailForce * Math.abs(Math.sin(sailAngle));
+    const driftSailForce = sailForce * Math.abs(Math.cos(sailAngle));
+    let fwdForce = fwdSailForce + windSpeed * Math.cos(windAngle) * KB;
+    let driftForce = (driftSailForce + Math.sign(-windAngle) * windSpeed * Math.abs(Math.sin(windAngle)) * KB ) * [0.1, 0.05, 0.01][keelPosition];
+    const waterResistance = -R[Math.sign(boatSpeed)+1] * boatSpeed * boatSpeed;
+    displayForce(fwdForce, driftForce);
     displayWaterResistance(waterResistance);
-    const rudderForce = KW * Math.sin(steeringAngle) * boatSpeed / 10;
+    const rudderForce = -KW * Math.abs(Math.sin(steeringAngle)) * boatSpeed;
+    const rudderTorque = -rudderForce * Math.sign(steeringAngle); // * Math.cos(steeringAngle);
+
+    displayRudderTorque(rudderTorque);
+    boatDirection = (boatDirection - rudderTorque / M * DT) % (2*Math.PI);
     // Compute boat acceleration
-    const acceleration = (sailForce - rudderForce - waterResistance) / M;
+    let fwdAcceleration = (fwdForce + rudderForce + waterResistance) / M;
+    // acceleration = Math.abs(acceleration) < 1 ? 0 : acceleration;
+    displayAcceleration(fwdAcceleration);
     // Update boat speed and direction
-    boatSpeed += acceleration * DT;
-    displayBoatSpeed(boatSpeed);
+    boatSpeed += fwdAcceleration * DT;
+    boatDrift = driftForce / M;
+    displayBoatSpeed(boatSpeed, boatDrift);
+    boatPosition.x += boatSpeed * Math.cos(boatDirection) * DT + boatDrift * Math.sin(boatDirection) * DT;
+    boatPosition.y += boatSpeed * Math.sin(boatDirection) * DT + boatDrift * Math.cos(boatDirection) * DT;
+    displayBoatPosition(boatPosition.x, boatPosition.y);
     ctx.restore();
     ctx.save();
     ctx.rotate(boatDirection);
@@ -197,7 +230,8 @@ scratchCtx.textBaseline = 'middle';
 scratchCtx.save();
 
 
-
+// draw NESW letters and circle and lines marking 4 additional directions, and cross-lines to represent water
+// relative to boat as it moves.
 function drawNESW(r) {
     r = (r || Math.PI/4) + Math.PI/2;
     // Draw rotated letters on scratch canvas and copy to main canvas
@@ -228,6 +262,14 @@ function drawNESW(r) {
         ctx.lineTo((s+15) * Math.cos(x), -(s+15) * Math.sin(x));
     }
     ctx.stroke();
+    ctx.beginPath();
+    ctx.setLineDash([4,10]);
+    ctx.moveTo((-boatPosition.x + canvas.width/2) % canvas.width - canvas.width/2, -canvas.height/2);
+    ctx.lineTo((-boatPosition.x + canvas.width/2) % canvas.width - canvas.width/2, canvas.height/2);
+    let x = (boatPosition.y + canvas.height/2) % canvas.height - canvas.height/2;
+    ctx.moveTo(-canvas.width/2, x); //(boatPosition.y + canvas.height/2) % canvas.height - canvas.height/2);
+    ctx.lineTo(canvas.width/2, (boatPosition.y + canvas.height/2) % canvas.height - canvas.height/2);
+    ctx.stroke();
 }
 
 function init() {
@@ -257,6 +299,10 @@ function init() {
 
     document.getElementById('wind-speed').addEventListener('input', function(event) { // alert("wind speed");
         windSpeed = event.target.value * maxWindSpeed;
+    });
+
+    document.getElementById('keel-position').addEventListener('input', function(event) { // alert("wind speed");
+        keelPosition = event.target.value;
     });
 
     // animation loop:  
