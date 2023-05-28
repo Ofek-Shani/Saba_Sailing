212033b54c819355c9bfbd43df18cce0d146cb08
KingDee241 <57509042+KingDee241@users.noreply.github.com>
2023-05-28 12:00:53 -0400

    implement rudder and keel control, as well as choosing which thing to control using number keys

---
+++ Assets/Scripts/BoatController.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+
+    float windSpeed, windDirection, rudderAngle, sailAngle, keelPosition, boatAngle = 0;
+
+    enum BoatPart { KEEL, RUDDER, FRONTSAIL, MAINSAIL };
+    BoatPart boatPart = BoatPart.KEEL;
+    enum KeelStatus { UP, HALFWAY, DOWN };
+    KeelStatus keelStatus = KeelStatus.UP;
+
+    bool controlBothSails = true;
+
+    [SerializeField] GameObject frontSail, mainSail, rudder, keel;
+    SpriteRenderer frontSailSpr, mainSailSpr, rudderSpr, keelSpr;
+    [SerializeField] Sprite[] keelSprites;
+        
+
+    WindController wc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        windSpeed = 10;
+        windDirection = Mathf.PI / 2;
+        //mainSail = transform.GetChild(0).gameObject;
+        //frontSail = transform.GetChild(1).gameObject;
+
+        wc = GameObject.FindGameObjectWithTag("Wind").GetComponent<WindController>();
+
+        keelSpr = keel.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetControls();
+        //ControlSails();
+        
+    }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if(e.type.Equals(EventType.KeyUp))
+        {
+            switch(e.keyCode)
+            {
+                case KeyCode.Alpha1: boatPart = BoatPart.KEEL; break;
+                case KeyCode.Alpha2: boatPart = BoatPart.RUDDER; break;
+                case KeyCode.Alpha3: boatPart = BoatPart.FRONTSAIL; break;
+                case KeyCode.Alpha4: boatPart = BoatPart.MAINSAIL; break;
+                default: break;
+            }
+        }
+    }
+
+    void GetControls()
+    {
+        //Debug.Log(boatPart);
+        switch(boatPart)
+        {
+            case BoatPart.KEEL: ControlKeel(); break;
+            case BoatPart.RUDDER: ControlRudder(); break;
+            case BoatPart.FRONTSAIL: ControlSails(); break;
+            case BoatPart.MAINSAIL: ControlSails(); break;
+            default: break;
+        }
+    }
+
+    void CalculateSpeed()
+    {
+
+    }
+
+    void ControlKeel()
+    {
+        // KeelStatus 0 is fully up and not in water, 2 is down and fully in water
+        Debug.Log("Controlling Keel...");
+        if(Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W)) // move keel up
+        {
+            if(keelStatus != KeelStatus.UP ) keelStatus--;
+            keelSpr.sprite = keelSprites[(int)keelStatus];
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S)) // move keel down
+        {
+            if (keelStatus != KeelStatus.DOWN) keelStatus++;
+            keelSpr.sprite = keelSprites[(int)keelStatus];
+        }
+    }
+
+    void ControlRudder()
+    {
+        Debug.Log("Controlling Rudder...");
+        float horizInput = Input.GetAxis("Horizontal");
+        rudderAngle += horizInput * .1f;
+
+        rudderAngle = Mathf.Clamp(rudderAngle, -80, 80);
+
+        rudder.transform.localRotation = Quaternion.Euler(0, 0, rudderAngle);
+    }
+
+    void ControlSails()
+    {
+        Debug.Log("Controlling Sails...");
+        float horizInput = Input.GetAxis("Horizontal");
+        sailAngle += horizInput * .1f;
+
+        sailAngle = Mathf.Clamp(sailAngle, -150, -30);
+
+        frontSail.transform.localRotation = Quaternion.Euler(0, 0, sailAngle);
+        mainSail.transform.localRotation = Quaternion.Euler(0, 0, sailAngle);
+
+        CalculateSailShape();
+    }
+
+    void CalculateSailShape()
+    {
+        float frontSailZ = frontSail.transform.rotation.eulerAngles.z;
+        float mainSailZ = mainSail.transform.rotation.eulerAngles.z;
+        float[] GetForces(float sailDir)
+        {
+            float deltaAngle = (sailDir - wc.windDirection + 360) % 360;
+            float forceTangent = Mathf.Cos(deltaAngle * Mathf.Deg2Rad);
+            float forceNormal = -1 * Mathf.Sin(deltaAngle * Mathf.Deg2Rad);
+
+            return new float[] { deltaAngle, forceTangent, forceNormal };
+        }
+
+        float[] mainForces = GetForces(mainSailZ);
+        float[] frontForces = GetForces(frontSailZ);
+
+        frontSail.transform.localScale = new Vector3(frontSail.transform.localScale.x, frontForces[2], 0);
+        mainSail.transform.localScale = new Vector3(mainSail.transform.localScale.x, mainForces[2], 0);
+    }
+
+
+}
--- Assets/Scripts/WindController.cs
+++ Assets/Scripts/WindController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class WindController : MonoBehaviour
 {
-
+    [SerializeField] float displacement = 3;
     public float windDirection = 45;
     public int windStrength = 10;
     ParticleSystem pt;
@@ -19,6 +19,7 @@ public class WindController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float wdr = windDirection * Mathf.Deg2Rad;
         Vector2 windDirectionVector = new Vector2(Mathf.Cos(Mathf.Deg2Rad * windDirection), Mathf.Sin(Mathf.Deg2Rad * windDirection)) * windStrength;
 
         ParticleSystem.Particle[] particles = new ParticleSystem.Particle[pt.particleCount];
@@ -31,5 +32,7 @@ public class WindController : MonoBehaviour
         }
 
         pt.SetParticles(particles, pt.particleCount);
+
+        gameObject.transform.localPosition = new Vector3(Mathf.Cos(wdr), Mathf.Sin(wdr), -3) * -1 * displacement;
     }
 }
