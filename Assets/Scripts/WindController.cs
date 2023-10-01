2306c6e50fd6d5c7541ce1333812a47c7a795c2c
Uri Shani <uri.shani@gmail.com>
2023-10-01 16:41:23 +0300

    adding on-screen ui and modify wind particles

--- Assets/Scripts/WindController.cs
+++ Assets/Scripts/WindController.cs
@@ -9,6 +9,7 @@ public class WindController : MonoBehaviour
     public float windDirection = 0;
     public int windStrength = 10;
     ParticleSystem pt;
+    ParticleSystem.EmissionModule emissionModule;
 
     public static WindController instance;
 
@@ -21,6 +22,7 @@ public class WindController : MonoBehaviour
     void Start()
     {
         pt = GetComponent<ParticleSystem>();
+        emissionModule = pt.emission;
     }
 
     // Update is called once per frame
@@ -28,18 +30,19 @@ public class WindController : MonoBehaviour
     {
         float wdr = windDirection * Mathf.Deg2Rad;
         Vector2 windDirectionVector = new Vector2(Mathf.Cos(Mathf.Deg2Rad * windDirection), Mathf.Sin(Mathf.Deg2Rad * windDirection)) * windStrength;
-
+        emissionModule.rateOverTime = windStrength/2;
         ParticleSystem.Particle[] particles = new ParticleSystem.Particle[pt.particleCount];
-
+        
         pt.GetParticles(particles);
-
-        for(int i = 0; i < particles.Length; i++)
+        float speed = windStrength * 0.001f * 0.1f; // .5f;
+        Vector2 velocity = windDirectionVector * speed;
+        for (int i = 0; i < particles.Length; i++)
         {
-            particles[i].velocity = windDirectionVector * windStrength * Time.deltaTime * .5f;
+            particles[i].velocity = velocity;
         }
+        Debug.Log(speed); //Debug.Log(windStrength);
+        pt.SetParticles(particles, pt.particleCount); // pt.particleCount * 100);
 
-        pt.SetParticles(particles, pt.particleCount);
-
-        gameObject.transform.localPosition = new Vector3(Mathf.Cos(wdr), Mathf.Sin(wdr), -3) * -1 * displacement;
+        gameObject.transform.localPosition = new Vector3(Mathf.Cos(wdr), Mathf.Sin(wdr), 1) * -1 * displacement;
     }
 }
